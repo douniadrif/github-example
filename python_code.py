d51b9623f603b236acc4addcaa3e4f0f294e8495
douniadrif <62078775+douniadrif@users.noreply.github.com>
2020-03-11 22:20:39 +0100

    Create python_code.py

---
+++ python_code.py
@@ -0,0 +1,64 @@
+{
+    "nbformat_minor": 1, 
+    "cells": [
+        {
+            "source": "# Capstone Project Notebook (IBM Data Science - Course 9 - Lab1)", 
+            "cell_type": "markdown", 
+            "metadata": {
+                "collapsed": true
+            }
+        }, 
+        {
+            "source": "Create a new repository on your Github account and name it Coursera_Capstone. I have prepared a guide to walk you through the process of creating a repository and setting it up. For Mac users, click here. For Windows users, click here.\n\nNow, start a Jupyter Notebook using any platform that you are comfortable with and do the following:\n\n    Write some markdown to explain that this notebook will be mainly used for the capstone project.\n    Import the pandas library as pd.\n    Import the Numpy library as np.\n    Print the following the statement: Hello Capstone Project Course!\n\nPush the Notebook to your Github repository and submit a link to the notebook on your Github repository.", 
+            "cell_type": "markdown", 
+            "metadata": {}
+        }, 
+        {
+            "source": "This **Capstone Project** is the first lab from the *Course 9 - Applied Data Science Capstone*.", 
+            "cell_type": "markdown", 
+            "metadata": {}
+        }, 
+        {
+            "execution_count": 4, 
+            "cell_type": "code", 
+            "metadata": {}, 
+            "outputs": [
+                {
+                    "output_type": "stream", 
+                    "name": "stdout", 
+                    "text": "Hello Capstone Project Course!\n"
+                }
+            ], 
+            "source": "import pandas as pd\nimport numpy as np\nprint(\"Hello Capstone Project Course!\")"
+        }, 
+        {
+            "execution_count": null, 
+            "cell_type": "code", 
+            "metadata": {}, 
+            "outputs": [], 
+            "source": ""
+        }
+    ], 
+    "metadata": {
+        "kernelspec": {
+            "display_name": "Python 3.5", 
+            "name": "python3", 
+            "language": "python"
+        }, 
+        "language_info": {
+            "mimetype": "text/x-python", 
+            "nbconvert_exporter": "python", 
+            "version": "3.5.5", 
+            "name": "python", 
+            "file_extension": ".py", 
+            "pygments_lexer": "ipython3", 
+            "codemirror_mode": {
+                "version": 3, 
+                "name": "ipython"
+            }
+        }
+    }, 
+    "nbformat": 4
+}
+@douniadrif
+ 
